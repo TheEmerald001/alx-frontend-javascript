04d38043fff3da4b7df07eb020e080828d7f8a78
steve <austines811@gmail.com>
2023-08-17 15:14:48 +0300

    adds tested functions for data manipulation tasks

---
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -0,0 +1,20 @@
+export default function updateStudentGradeByCity(
+  studentsArray,
+  city,
+  newGrades,
+) {
+  const students = studentsArray.filter((student) => student.location === city);
+  for (const student of students) {
+    student.grade = 'N/A';
+  }
+  const newStudents = students.map((student) => {
+    const single = student;
+    for (const grade of newGrades) {
+      if (single.id === grade.studentId) {
+        single.grade = grade.grade;
+      }
+    }
+    return single;
+  });
+  return newStudents;
+}
